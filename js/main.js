46c2a97f0a393909609d85442907dab87b23e3e8
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-06-19 03:20:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,6 +88,51 @@ const clearImageAtributOnModal = () => {
 // end close modal
 
 
-btnLeft.addEventListener('click', slider);
-btnRight.addEventListener('click', slider);
+// btnLeft.addEventListener('click', slider);
+// btnRight.addEventListener('click', slider);
 
+
+// function slider(event) {
+//   // const index = +lightboxImage.dataset.index;
+//   // const imageEl = gallery.querySelectorAll('.gallery__image');
+
+//   // if (event.target.dataset.action === 'slider-left') {
+//   //   lightboxImage.src = imageEl[index - 1].dataset.source;
+//   //   lightboxImage.dataset.index = imageEl[index - 1].dataset.index;
+//   //   lightboxImage.alt = imageEl[index - 1].alt;
+//   //   checkButtonStatus(index - 1);
+//   // } else {
+//   //   lightboxImage.src = imageEl[index + 1].dataset.source;
+//   //   lightboxImage.dataset.index = imageEl[index + 1].dataset.index;
+//   //   lightboxImage.alt = imageEl[index + 1].alt;
+//   //   checkButtonStatus(index + 1);
+//   // }
+// }
+
+// function checkButtonStatus(index) {
+//   btnLeft.disabled = index === 0;
+//   btnRight.disabled = index === gallery.children.length - 1;
+// }
+
+function slider(event) {
+
+    let imgIndex = galleryItems.findIndex(img => img.original === lightboxImage.src);
+
+    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown' || btnLeft === event.target) {
+        if (imgIndex === 0) {
+            imgIndex += galleryItems.length;
+        }
+        imgIndex -= 1;
+    };
+
+    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || btnRight === event.target /*|| modalContent === event.target*/) {
+        if (imgIndex === galleryItems.length - 1) {
+            imgIndex -= galleryItems.length;
+        }
+        imgIndex += 1;
+    };
+
+    modalImg.src = galleryItems[imgIndex].original;
+    modalImg.alt = galleryItems[imgIndex].description;
+
+};
